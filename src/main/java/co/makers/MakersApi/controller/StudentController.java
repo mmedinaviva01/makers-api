186790a1df639e7474d9b85f0a447630f6ada2cd
mmedina <mmedina@chafer.com.co>
2024-04-22 20:37:14 -0500

    Revert "Finalización api makers"
    This reverts commit 60ce5c3742117c70da40d1b7f83231aee456f1d0.

--- src/main/java/co/makers/MakersApi/controller/StudentController.java
+++ src/main/java/co/makers/MakersApi/controller/StudentController.java
@@ -1,97 +1,25 @@
 package co.makers.MakersApi.controller;
 
-
-import co.makers.MakersApi.model.Student;
-import co.makers.MakersApi.repository.StudentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/students")
 public class StudentController {
-    private final StudentRepository studentRepository;
-
-    @Autowired
-    public StudentController(StudentRepository studentRepository) {
-        this.studentRepository = studentRepository;
-    }
+    private final StudentRespository studentRespository;
+    private final EntityLinks entityLinks;
 
     @GetMapping
     public ResponseEntity<CollectionModel<EntityModel<Student>>> getAllStudents() {
         List<EntityModel<Student>> students = studentRepository.findAll().stream()
                 .map(student -> EntityModel.of(student,
-                        linkTo(methodOn(StudentController.class).getStudentById(student.getStudentID())).withRel("self"),
-                        linkTo(methodOn(StudentController.class).getStudentById(student.getStudentID())).withRel("students")))
+                        entityLinks.linkToItemResource(Student.class, student.getId()).withRel("self"),
+                        entityLinks.linkToCollectionResource(Student.class).withRel("students")))
                 .collect(Collectors.toList());
-        if (students.isEmpty())
-            return ResponseEntity.notFound().build();
-        else
-            return ResponseEntity.ok(CollectionModel.of(students));
+
+        return ResponseEntity.ok(CollectionModel.of(students,
+                entityLinks.linkToCollectionResource(Student.class).withSelfRel()));
     }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<EntityModel<Student>> getStudentById(@PathVariable Long id) {
-        return studentRepository.findById(id)
-                .map(student -> EntityModel.of(student,
-                        linkTo(methodOn(StudentController.class).getStudentById(student.getStudentID())).withSelfRel(),
-                        linkTo(methodOn(StudentController.class).getStudentById(student.getStudentID())).withRel("students")))
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
-    }
-
-    @PostMapping
-    public ResponseEntity<EntityModel<Student>> createStudent(@RequestBody Student student) {
-        try {
-            Student savedStudent = studentRepository.save(student);
-
-            EntityModel<Student> studentResource = EntityModel.of(savedStudent, linkTo(methodOn(StudentController.class).getStudentById(savedStudent.getStudentID())).withSelfRel(),
-                    linkTo(methodOn(StudentController.class)).withRel("students"));
-            return ResponseEntity.created(studentResource.getRequiredLink("self").toUri()).body(studentResource);
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error creating student", e);
-        }
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<EntityModel<Student>> updateStudent(@PathVariable Long id, @RequestBody Student currentStudent) {
-        return studentRepository.findById(id)
-                .map(student -> {
-                    student.setFirstName(currentStudent.getFirstName());
-                    student.setLastName(currentStudent.getLastName());
-                    student.setEmail(currentStudent.getEmail());
-
-                    Student updatedStudent = studentRepository.save(student);
-
-                    EntityModel<Student> studentResource = EntityModel.of(updatedStudent, linkTo(methodOn(StudentController.class).getStudentById(updatedStudent.getStudentID())).withSelfRel(),
-                            linkTo(methodOn(StudentController.class)).withRel("students"));
-                    return ResponseEntity.ok(studentResource);
-                }).orElseGet(() -> {
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-                });
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteStudent(@PathVariable Long id) {
-        return studentRepository.findById(id)
-                .map(student -> {
-                    studentRepository.delete(student);
-                    return ResponseEntity.noContent().build();
-                }).orElseGet(() -> ResponseEntity.notFound().build());
-    }
-
 }
